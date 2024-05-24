ee59de56fba291d888464bbad5a6b30effccbfb7
Arpan Rijal <135034681+Mr-ARP@users.noreply.github.com>
2024-05-24 20:53:55 +0545

    Add files via upload
    this is the approved file with 100% fixes which may be implemented in home but if not then check this file ani yo file hera hai

---
+++ test.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <conio.h> //getch() use garna chainxa
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+char arr[3][3];
+bool player = true;
+int posi;
+int a[9];
+char currentPlayer;
+int endState = 0;
+char c;
+void location(int *x, int *y)
+{
+    string input;
+    if (*x == -1 && *y == -1)
+    {
+    pushback:
+        cout << endl
+             << "Enter the position from 0-9: ";
+        while ((c = getch()) != 13) // input.length ley 1 block space rakhney vayo
+        {
+            if (c >= 48 && c <= 57 && input.length() < 1 && input.length() !=1)
+            {
+                input.push_back(c);
+                cout << c;
+            }
+            if (input.length() > 0)
+            {
+                if (c == 8)
+                {
+                    input.pop_back();
+                    cout << "\b \b";
+                }
+            }
+            else if (c == 13)
+            {
+                cout<<endl<<"!! Enter other enter string!! ";
+                continue;
+            }
+        }
+        posi = atoi(input.data());
+        c='\0';
+        input="\0";
+        if (posi >= 1 && posi <= 9)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (a[i] == posi)
+                {
+                    cout << endl
+                         << "!! Aleady Used, Please select Position between 1-9 except " << posi << " !!" << endl;
+                    // posi=0;
+                    goto pushback;
+                }
+            }
+        }
+    }
+    if (posi == 1 || posi == 2 || posi == 3)
+    {
+        *x = 0;
+        *y = posi - 1;
+        a[posi] = posi; // 1-9 value aayo vaney matrai store garney natra store nai na garney
+    }
+    if (posi == 4 || posi == 5 || posi == 6)
+    {
+        *x = 1;
+        *y = posi - 4;
+        a[posi] = posi;
+    }
+    if (posi == 7 || posi == 8 || posi == 9)
+    {
+        *x = 2;
+        *y = posi - 7;
+        a[posi] = posi;
+    }
+    if (posi <= 0 || posi >= 10)
+    {
+        int a = -1;
+        cout << endl
+             << "!! Please enter the position between 1-9 !!" << endl;
+        location(&a, &a);
+    }
+}
+char currentPlayer_Is()
+{
+    char c;
+    string input;
+    cout << endl
+         << "Enter your Player either X or O: ";
+    while ((c = getch()) != 13 || input.length() < 1)
+    {
+        if (c == 13)
+        {
+            continue;
+        }
+        if (input.length() >= 1 && input.length() != 0)
+        {
+            if (c == 8)
+            {
+                input.pop_back();
+                cout << "\b \b";
+            }
+        }
+        if ((c == 88 || c == 120) || (c == 79 || c == 111))
+        {
+            if (input.length() < 1)
+            {
+                input.push_back(c);
+                cout << c;
+
+                if ((c == 88 || c == 120))
+                {
+                    currentPlayer = 88;
+                }
+                if ((c == 79 || c == 111))
+                {
+                    currentPlayer = 111;
+                }
+            }
+        }
+    }
+    if (currentPlayer == 'O' || currentPlayer == 'o')
+    {
+        player = false;
+    }
+    else if (currentPlayer == 'X' || currentPlayer == 'x')
+    {
+        player = false;
+    }
+    else
+    {
+        cout << endl
+             << "!! Please select either X or O !!" << endl
+             << endl;
+    }
+    if ((currentPlayer == 88 || currentPlayer == 120) || (currentPlayer == 79 || currentPlayer == 111))
+    {
+        return currentPlayer;
+    }
+    else
+    {
+        return currentPlayer_Is();
+    }
+}
+void boxesOnly(int why)
+{
+        if(why==1)
+        {
+        currentPlayer_Is(); // first time x or o chhose garna deko user lai
+        }
+        cout << endl
+             << endl;
+        cout << endl
+             << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cout << endl;
+            for (int j = 0; j < 3; j++)
+            {
+                if (j == 1 || j == 2)
+                {
+                    cout << " ";
+                    cout << arr[i][j] << " ";
+                }
+                else
+                {
+                    cout << "   ";
+                    cout << arr[i][j] << " ";
+                }
+                if (j == 2)
+                {
+                    cout << "  ";
+                }
+                else
+                {
+                    cout << "|";
+                }
+            }
+            cout << endl;
+            cout << "   ";
+            for (int k = 0; k < 3; k++)
+            {
+                if (i >= 2)
+                {
+                    break;
+                }
+                cout << "---";
+            }
+        }
+}
+void playMe(int firstTime)
+{
+     if (firstTime == 1)
+    {
+        boxesOnly(1);
+        playMe(0);
+    }
+    else
+    {
+        int x = -1, y = -1;
+        for (int i = 0; i < 3; i++) // position ma X or O ko position set garna khoj da yo rakhne ho
+        {
+            location(&x, &y); //1-9 input lina use hunxa
+            if (firstTime == 0)
+            {
+                system("cls");
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                if (player)
+                {
+                    arr[x][y] = 'X';
+                    player = false;
+                }
+                else
+                {
+                    arr[x][y] = 'O';
+                    player = true;
+                }
+            }
+        }
+        endState++;
+    }
+            boxesOnly(0); //yo chan pattern print garna ko lagi matrai ho wit X or O if any available
+    if (endState != 9) //yo chai 9 patak vanda badi input na lige game lai close garna lai ho
+    {
+        playMe(0);
+    }
+}
+int main()
+{
+    playMe(1);
+    return 0;
+}
